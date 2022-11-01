7223e7d17dbe026c8aecea5a48d1ae32c03e132c
Edwin574 <owinoedwin574@gmail.com>
2022-11-01 09:43:08 +0300

    initial commit

---
+++ rock_Paper.py
@@ -0,0 +1,34 @@
+import random
+from tkinter import E
+
+choices= ['rock','paper','scissors']
+
+computer= random.choice(choices)
+player=None
+
+while player not in choices:
+    player=input('Rock,Paper or Scissors?:').lower()
+
+    if computer==player:
+        print('computer:',computer)
+        print('Player:',player)
+        print('This is a tie')
+    else: 
+        if computer=='rock':
+            if player=='paper':
+                print("Computer:",computer)
+                print("Player:",player)
+                print('player wins')
+        else:
+            if computer=='paper':
+                if player=='rock':
+                    print("Computer:",computer)
+                    print("Player:",player)
+                    print('computer wins')
+            else:
+                if computer=='scissors':
+                    if player=='rock':
+                        print("Computer:",computer)
+                        print("Player:",player)
+                        print('player wins')
+             
