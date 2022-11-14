4cb9e9b16dd728f5e802355075f34ca2bc1463a8
Edwin574 <owinoedwin574@gmail.com>
2022-11-14 16:09:46 +0300

    final game

--- rock_Paper.py
+++ rock_Paper.py
@@ -8,47 +8,44 @@ player=None
 
 while player not in choices:
     player=input('Rock,Paper or Scissors?:').lower()
+    print(computer)
 
     if computer==player:
         print('computer:',computer)
         print('Player:',player)
         print('This is a tie')
-    else: 
-        if computer=='rock':
+    elif computer=='rock':
             if player=='paper':
                 print("Computer:",computer)
                 print("Player:",player)
                 print('player wins')
-        else:
-            if computer=='paper':
-                if player=='rock':
+            else:
+                if player=='scissors':
                     print("Computer:",computer)
                     print("Player:",player)
                     print('computer wins')
-            else:
-                if computer=='scissors':
-                    if player=='rock':
-                        print("Computer:",computer)
-                        print("Player:",player)
-                        print('player wins')
-                else:
-                    if player=='scissors':
-                        if computer=='rock':
-                            print('Computer',computer)
-                            print('Player',player)
-                            print('Computer wins')
-                    else:
-                        if player=='paper':
-                            if computer=='rock':
-                                print('Computer',computer)
-                                print('Player',player)
-                                print('Player wins')
-                        else:
-                            if player=='paper':
-                                if computer=='scissors':
-                                      print('Computer',computer)
-                                      print('Player',player)
-                                      print('Player wins')
+    elif computer=='paper':
+        if player=='rock':
+            print("Computer:",computer)
+            print("Player:",player)
+            print('computer wins')
+        else:
+            if player=='scissors':
+                print('Computer',computer)
+                print('Player',player)
+                print('player wins')
+    else:
+        if player=='paper':
+                
+            print('Computer',computer)
+            print('Player',player)
+            print('computer wins')
+        else: 
+           if player=='rock':
+            print('Computer',computer)
+            print('Player',player)
+            print('Player wins')
+               
                     
 
              
